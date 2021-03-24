bec7869463d2dffb55a69e8a01c23f7fb940fcf6
Cosmin <cossmc93@gmail.com>
2021-03-24 19:06:44 +0200

    DDOS discord servers just so we can achieve real time playback

--- bad apple discord player.py
+++ bad apple discord player.py
@@ -1,34 +1,42 @@
-import discord
+import asyncio
+import functools
+import pathlib
 import time
+
+import discord
 from PIL import Image
 
 CLIP_FRAMES = 6571
 
 CLIP_LENGTH = 219.0666
 
-ASCII_CHARS = ['⠀','⠄','⠆','⠖','⠶','⡶','⣩','⣪','⣫','⣾','⣿']
+ASCII_CHARS = ['⠀', '⠄', '⠆', '⠖', '⠶', '⡶', '⣩', '⣪', '⣫', '⣾', '⣿']
 ASCII_CHARS.reverse()
 ASCII_CHARS = ASCII_CHARS[::-1]
 
 WIDTH = 60
+FPS = CLIP_FRAMES / CLIP_LENGTH
+TIMEOUT = 1 / FPS
 
-TIMEOUT = 1/((int(CLIP_FRAMES/4)+1)/CLIP_LENGTH)*18
 
 def resize(image, new_width=WIDTH):
     (old_width, old_height) = image.size
-    aspect_ratio = float(old_height)/float(old_width)
-    new_height = int((aspect_ratio * new_width)/2)
+    aspect_ratio = float(old_height) / float(old_width)
+    new_height = int((aspect_ratio * new_width) / 2)
     new_dim = (new_width, new_height)
     new_image = image.resize(new_dim)
     return new_image
 
+
 def grayscalify(image):
     return image.convert('L')
 
+
 def modify(image, buckets=25):
     initial_pixels = list(image.getdata())
-    new_pixels = [ASCII_CHARS[pixel_value//buckets] for pixel_value in initial_pixels]
+    new_pixels = [ASCII_CHARS[pixel_value // buckets] for pixel_value in initial_pixels]
     return ''.join(new_pixels)
+
 
 def do(image, new_width=WIDTH):
     image = resize(image)
@@ -37,61 +45,96 @@ def do(image, new_width=WIDTH):
     pixels = modify(image)
     len_pixels = len(pixels)
 
-    new_image = [pixels[index:index+int(new_width)] for index in range(0, len_pixels, int(new_width))]
+    new_image = [pixels[index:index + int(new_width)] for index in range(0, len_pixels, int(new_width))]
 
     return '\n'.join(new_image)
 
+
 def runner(path):
-    image = None
     try:
         image = Image.open(path)
     except Exception:
-        print("Unable to find image in",path)
+        print("Unable to find image in", path)
         return
     image = do(image)
 
     return image
 
+
 frames = []
 
-for i in range(0, int(CLIP_FRAMES/4)+1):
-    path = "frames/frame"+str(i*4)+".jpg" #<--- path to folder containing every frame of the video
+for i in range(0, CLIP_FRAMES):
+    frames_dir = pathlib.Path() / "frames"
+    path = frames_dir / f"frame{i}.jpg"  # <--- path to folder containing every frame of the video
     frames.append(runner(path))
 
-client = discord.Client()
+FRAME_COUNTER = 0
+PREVIOUS_FRAME = None
+BOT_COUNT = 0
+FRAME_TIMES = []
 
-@client.event
-async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
 
-@client.event
-async def on_message(message):
-
+async def on_message(bot_number, message):
+    global FRAME_COUNTER
+    global BOT_COUNT
+    global PREVIOUS_FRAME
     if message.content.startswith('!bad apple'):
-        
-        oldTimestamp = time.time()
+        initial_timestamp = time.time()
+        while FRAME_COUNTER < len(frames) - 1:
+            await asyncio.sleep(0.005)
+            if BOT_COUNT % len(BOT_IDS) != bot_number:
+                continue
+            while True:
+                new_timestamp = time.time()
+                FRAME_TIMES.append((new_timestamp - initial_timestamp, new_timestamp, initial_timestamp))
+                FRAME_COUNTER = int((new_timestamp - initial_timestamp) / TIMEOUT)
+                if FRAME_COUNTER == PREVIOUS_FRAME:
+                    continue
+                PREVIOUS_FRAME = FRAME_COUNTER
+                if FRAME_COUNTER >= len(frames):
+                    break
+                await message.channel.send(frames[FRAME_COUNTER])
+                BOT_COUNT = (BOT_COUNT + 1) % len(BOT_IDS)
+                break
+        FRAME_COUNTER = 0
+        PREVIOUS_FRAME = None
+        BOT_COUNT = 0
 
-        start = oldTimestamp
 
-        seconds = 0
-        minutes = 0
+BOT_1_ID = ''
+BOT_2_ID = ''
+BOT_3_ID = ''
+BOT_4_ID = ''
+BOT_5_ID = ''
+BOT_6_ID = ''
+BOT_7_ID = ''
+BOT_8_ID = ''
+BOT_IDS = [BOT_1_ID, BOT_2_ID, BOT_3_ID, BOT_4_ID, BOT_5_ID, BOT_6_ID, BOT_7_ID, BOT_8_ID]
+threads = []
 
-        i = 0
-        
-        while i < len(frames)-1:
-            disp = False
-            while not disp:
-                newTimestamp = time.time()
-                if (newTimestamp - oldTimestamp) >= TIMEOUT:
 
-                    await message.channel.send(frames[int(i)])
-                    
-                    newTimestamp = time.time()
+async def on_ready(client, bot_number):
+    print(f'We have logged in as {client.user} {bot_number}')
 
-                    i += (newTimestamp - oldTimestamp)/TIMEOUT
-                    
-                    oldTimestamp = newTimestamp
 
-                    disp = True
+def build_client(event_loop, i):
+    client = discord.Client(loop=event_loop)
+    ready_callback = functools.partial(on_ready, client, i)
+    ready_callback.__name__ = "on_ready"
+    message_callback = functools.partial(on_message, i)
+    message_callback.__name__ = "on_message"
+    client.event(coro=ready_callback)
+    client.event(coro=message_callback)
+    return client
 
-client.run('')#<--- Put bot token here
+
+async def main():
+    event_loop = asyncio.get_event_loop()
+    tasks = []
+    for i, bot_id in enumerate(BOT_IDS):
+        client = build_client(event_loop, i)
+        tasks.append(asyncio.ensure_future(client.start(bot_id)))
+    await asyncio.wait(tasks)
+
+
+asyncio.run(main())
--- video to frames.py
+++ video to frames.py
@@ -1,21 +1,23 @@
-import cv2 
-  
-def FrameCapture(path): 
-    
-    vidObj = cv2.VideoCapture(path) 
-    
+import pathlib
+
+import cv2
+
+
+def FrameCapture(path):
+    vidObj = cv2.VideoCapture(path)
+    frames_dir = pathlib.Path("") / "frames"
+    frames_dir.mkdir(exist_ok=True)
     count = 0
-    
+
     success = 1
-  
-    while success: 
 
-        success, image = vidObj.read() 
-  
-        cv2.imwrite("frame%d.jpg" % count, image)
-  
+    while success:
+        success, image = vidObj.read()
+
+        cv2.imwrite(str(frames_dir / f"frame{count}.jpg"), image)
+
         count += 1
-  
-if __name__ == '__main__': 
 
+
+if __name__ == '__main__':
     FrameCapture("bad_apple.mp4")
